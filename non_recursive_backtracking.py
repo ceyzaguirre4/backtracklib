818e3c91f7ef7a8f74c985e6af3b9d5951f974d4
Cristobal Eyzaguirre <cristobaleyzaguirre@Cristobals-MBP.iptv.microsoft.com>
2017-06-03 20:55:10 -0400

    added a non recursive backtracker

---
+++ non_recursive_backtracking.py
@@ -0,0 +1,53 @@
+class NonSolver:
+	def __init__(self, CalcularPosibles, basecase):
+		self.CalcularPosibles = CalcularPosibles
+		self.basecase = basecase
+		self._solutions, self._time, self._found_all = None, None, None
+		self._parcial = []
+
+	@property
+	def solutions(self):
+		if not self._time:
+			return self._solve()
+
+	def _solve(self):
+		calcs = [self.CalcularPosibles(self._parcial)]
+		depth = 0
+		while not self.basecase(self._parcial):
+			if depth == len(calcs):
+				calcs.append(self.CalcularPosibles(self._parcial))
+			elif depth > len(calcs):
+				raise Exception("depth mayor que el largo")
+			posibles = calcs[depth]
+			if posibles:
+				self._parcial.append(posibles.pop())
+				depth += 1
+			else:
+				depth -= 1
+				if not depth:
+					break
+				self._parcial.pop()
+				calcs.pop()
+		return self._parcial
+
+n_queens = 3
+
+if __name__ == "__main__":
+	def basecase(parcial):
+		if len(parcial) == n_queens:
+			return True
+		return False
+
+	def calculate_posibles(parcial):
+		ret = []
+		for x in range(n_queens):
+			for y in range(n_queens):
+				is_in = False
+				for i in range(n_queens):
+					if (x,i) in parcial or (i, y) in parcial or (x-i, y-i) in parcial or (x+i, y+i) in parcial:
+						is_in = True
+				if not is_in: 
+					ret.append((x,y))
+		return ret
+	gen = NonSolver(calculate_posibles, basecase)
+	print(list(gen.solutions))
---
+++ test_3.py
@@ -0,0 +1,31 @@
+from non_recursive_backtracking import NonSolver
+
+# 8 queens problem: position 8 queens on a chessboard so that no one attacks another.
+
+def basecase(parcial):
+	if len(parcial) == 8:
+		return True
+	return False
+
+def calculate_posibles(parcial):
+	ret = []
+	for x in range(8):
+		for y in range(8):
+			is_in = False
+			for i in range(8):
+				if (x,i) in parcial or (i, y) in parcial or (x-i, y-i) in parcial or (x+i, y+i) in parcial:
+					is_in = True
+			if not is_in: 
+				ret.append((x,y))
+	return ret
+
+# uses a non recursive algorithm to backtrack
+
+gen = NonSolver(calculate_posibles, basecase)
+answer = gen.solutions
+
+def test_1():
+	assert basecase(answer)
+
+if __name__ == "__main__":
+	print(answer)
