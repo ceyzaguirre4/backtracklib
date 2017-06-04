5f9a694165ca9cac76c908a786fa33bf9c56e5a9
Cristobal Eyzaguirre <cristobaleyzaguirre@Cristobals-MBP.iptv.microsoft.com>
2017-06-03 21:13:22 -0400

    non_recursive return generator object

--- non_recursive_backtracking.py
+++ non_recursive_backtracking.py
@@ -2,18 +2,21 @@ class NonSolver:
 	def __init__(self, CalcularPosibles, basecase):
 		self.CalcularPosibles = CalcularPosibles
 		self.basecase = basecase
-		self._solutions, self._time, self._found_all = None, None, None
+		self._solutions, self._time, self._found_all = [], None, None
 		self._parcial = []
 
 	@property
 	def solutions(self):
-		if not self._time:
-			return self._solve()
+		return self._solve()
 
 	def _solve(self):
 		calcs = [self.CalcularPosibles(self._parcial)]
 		depth = 0
-		while not self.basecase(self._parcial):
+		while True:
+			if self.basecase(self._parcial):
+				copia = self._parcial.copy()
+				self._solutions.append(copia)
+				yield copia
 			if depth == len(calcs):
 				calcs.append(self.CalcularPosibles(self._parcial))
 			elif depth > len(calcs):
@@ -23,14 +26,14 @@ class NonSolver:
 				self._parcial.append(posibles.pop())
 				depth += 1
 			else:
-				depth -= 1
 				if not depth:
 					break
+				depth -= 1
 				self._parcial.pop()
 				calcs.pop()
 		return self._parcial
 
-n_queens = 3
+n_queens = 4
 
 if __name__ == "__main__":
 	def basecase(parcial):
@@ -50,4 +53,4 @@ if __name__ == "__main__":
 					ret.append((x,y))
 		return ret
 	gen = NonSolver(calculate_posibles, basecase)
-	print(list(gen.solutions))
+	print(next(gen.solutions))
