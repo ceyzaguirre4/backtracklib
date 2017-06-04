7a1ddbc56c39108ed0127a8552c7f2509761a187
Cristobal Eyzaguirre <cristobaleyzaguirre@Cristobals-MBP.iptv.microsoft.com>
2017-06-03 22:33:46 -0400

    minor changes to non_recursive

--- non_recursive_backtracking.py
+++ non_recursive_backtracking.py
@@ -2,32 +2,26 @@ class NonSolver:
 	def __init__(self, calculate_posibles, basecase):
 		self.calculate_posibles = calculate_posibles
 		self.basecase = basecase
-		self._solutions, self._time, self._found_all = [], None, None
-		self._parcial = []
-
-	@property
-	def solutions(self):
-		return self._solve()
+		self.solutions = self._solve()
 
 	def _solve(self):
-		calcs = [self.calculate_posibles(self._parcial)]
+		partial = []
+		calcs = [self.calculate_posibles(partial)]
 		depth = 0
 		while True:
-			if self.basecase(self._parcial):
-				copia = self._parcial.copy()
-				self._solutions.append(copia)
-				yield copia
+			if self.basecase(partial):
+				yield partial.copy()
 			if depth == len(calcs):
-				calcs.append(self.calculate_posibles(self._parcial))
+				calcs.append(self.calculate_posibles(partial))
 			posibles = calcs[depth]
 			if posibles:
-				self._parcial.append(posibles.pop())
+				partial.append(posibles.pop())
 				depth += 1
 			else:
 				if not depth:
 					break
 				depth -= 1
-				self._parcial.pop()
+				partial.pop()
 				calcs.pop()
 
 
@@ -35,18 +29,18 @@ if __name__ == "__main__":
 
 	n_queens = 8
 
-	def basecase(parcial):
-		if len(parcial) == n_queens:
+	def basecase(partial):
+		if len(partial) == n_queens:
 			return True
 		return False
 
-	def calculate_posibles(parcial):
+	def calculate_posibles(partial):
 		ret = []
 		for x in range(n_queens):
 			for y in range(n_queens):
 				is_in = False
 				for i in range(n_queens):
-					if (x,i) in parcial or (i, y) in parcial or (x-i, y-i) in parcial or (x+i, y+i) in parcial:
+					if (x,i) in partial or (i, y) in partial or (x-i, y-i) in partial or (x+i, y+i) in partial:
 						is_in = True
 				if not is_in: 
 					ret.append((x,y))
